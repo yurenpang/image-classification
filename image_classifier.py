1eff38dc9af78af31db395da0a2bc4aa1fe01484
yurenpang <ypang@macalester.edu>
2019-05-13 22:37:17 -0500

    Added accuracy plot features

--- image_classifier.py
+++ image_classifier.py
@@ -1,47 +1,76 @@
+"""
+Author: Yuren "Rock" Pang, Jufeng Yan
+Deep learning model for multi-class image classification task
+"""
+
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers import Dropout
-
+from keras.preprocessing.image import ImageDataGenerator
 from numpy.random import seed
-seed(1)
 from tensorflow import set_random_seed
-set_random_seed(2)
+import matplotlib.pyplot as plt
 
+# set seeds for numpy random and tensorflow backend
+seed(1)
+set_random_seed(3)
+
+# Initialize the classifier
 classifier = Sequential()
 
-# Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape=(64,64,3), activation="relu"))
+# Convolution, with 3*3 filter
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation="relu"))
 
 # Pooling using MaxPooling
-classifier.add(MaxPooling2D(pool_size=(2,2)))
-classifier.add(Dropout(0.2)) # Dropout
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Flatten
 classifier.add(Flatten())
 
-# Compile the CNN
-classifier.add(Dense(units = 128, activation='relu'))
+# Compile the CNN with 2 hidden layers
+# use softmax for multiple classes
+classifier.add(Dense(units=128, activation='relu'))
 classifier.add(Dropout(rate=0.2))
+classifier.add(Dense(64, activation="relu"))
+classifier.add(Dropout(rate=0.2))
+classifier.add(Dense(units=6, activation="softmax"))
 
-classifier.add(Dense(units = 6, activation="softmax"))
+# Compile the classifier with the loss function: cross-entropy and accuracy metric
+classifier.compile(optimizer='adam', loss="categorical_crossentropy", metrics=['accuracy'])
 
-classifier.compile(optimizer='adam', loss = "categorical_crossentropy", metrics=['accuracy'])
-
-
-from keras.preprocessing.image import ImageDataGenerator
-
+# Data augmentation
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('./intel-image-classification/seg_train', target_size=(64,64), batch_size=32,class_mode="categorical")
+# Get the training set and test set from correct directory
+training_set = train_datagen.flow_from_directory('../intel-image-classification/seg_train', target_size=(64,64), batch_size=32,class_mode="categorical")
+test_set = train_datagen.flow_from_directory("../intel-image-classification/seg_test", target_size=(64,64), batch_size=32, class_mode="categorical")
 
-test_set = train_datagen.flow_from_directory("./intel-image-classification/seg_test", target_size=(64,64), batch_size=32, class_mode="categorical")
-
-classifier.fit_generator(
-    training_set, steps_per_epoch=8000, epochs=10,validation_data=test_set, validation_steps=800
+# Train the model, return a History object
+history = classifier.fit_generator(
+    training_set, steps_per_epoch=1000, epochs=60, validation_data=test_set, validation_steps=800
 )
-
+# Store the model which takes hours to run
 classifier.save("my_model.h5")
+
+# Plot the accuracy and loss function
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
--- predict.py
+++ predict.py
@@ -1,13 +1,16 @@
-
+"""
+Author: Yuren "Rock" Pang
+Predict if a picture is in a particular class
+"""
 import numpy as np
 from keras.models import load_model
-
-model = load_model('my_model.h5')
-
 from keras.preprocessing import image
-test_image = image.load_img('./intel-image-classification/seg_pred/21820.jpg', target_size=(64,64))
+
+model = load_model('third_model.h5')
+
+test_image = image.load_img('../intel-image-classification/seg_pred/seg_pred/23842.jpg', target_size=(64, 64))
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
+test_image = np.expand_dims(test_image, axis=0)
 result = model.predict(test_image)
 
 category = ["buildings", "forest", "glacier", "mountain", "sea", "street"]
